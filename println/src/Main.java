92abe5286e5e59f305d4916d97d39d6941d690b2
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-09-18 15:37:04 -0300

    Initial commit

---
+++ println/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+// Crie uma variável do tipo string, atribua um valor e concatene o valor dessa variável a um texto padronizado pelo comando println.
+        String nome = "Lucas";
+        System.out.println("Olá, " + nome);
+    }
+}
